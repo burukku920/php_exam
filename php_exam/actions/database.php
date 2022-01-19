c51c0557d61ffe4ea3c6a33355b775aa17fab640
sofiane1234 <79451497+sofiane1234@users.noreply.github.com>
2022-01-19 14:26:52 +0100

    Update database.php

--- php_exam/actions/database.php
+++ php_exam/actions/database.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('sqlite:host=localhosst;dbname=php_exam_db;charset=utf8;', 'root', 'root');
+    $db = new mysqli("localhost", "root", "", "php_exam_db");
 }catch(Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
