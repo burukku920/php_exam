fe2800d5c478e54606584bc0dcae147b4618f94d
sofiane1234 <79451497+sofiane1234@users.noreply.github.com>
2022-01-19 22:47:23 +0100

    Add files via upload

---
+++ actions/registering.php
@@ -0,0 +1,42 @@
+<?php
+require('actions/database.php');
+
+//Si le formulaire est valide (bouton confirmer clique)
+if(isset($_POST['valider'])) {
+
+    //Si tous les champs ne sont pas vides
+    if(!empty($_POST['email']) AND !empty($_POST['username']) AND !empty($_POST['pass'])) {
+
+        //Informations de l'utilisateur
+        $userName = htmlspecialchars($_POST['username']);
+        $userMail = htmlspecialchars($_POST['email']);
+        $userPassword = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+
+        //Voir si le compte existe deja
+        $checkUser = $db->prepare('SELECT * FROM users WHERE username = ?');
+        $checkUser->execute(array($userName));
+        if ($checkUser->rowCount() == 0) {
+
+            //Insertion de l'utilisateur dans la database
+            $insertUser = $db->prepare('INSERT INTO users(username,pass,email)VALUES(?,?,?)');
+            $insertUser->execute(array($userName, $userPassword, $userMail));
+            
+            //Obtention des infos de l'utilisateur
+            $getUser = $db->prepare('SELECT id, username FROM users WHERE username = ?');
+            $getUser->execute(array($userName));
+            $aboutUser = $getUser->fetch();
+
+            //Authentification de l'utilisateur
+            $_SESSION['auth'] = true;
+            $_SESSION['id'] = $aboutUser['id'];
+            $_SESSION['username'] = $aboutUser['username'];
+
+            //Redirection sur la page Home
+            header ('Location: home.php');
+        } else {
+            $errmsg = "Erreur! Utilisateur deja existant";
+        }
+    } else {
+        $errmsg = "Erreur! Veuillez remplir tous les champs!";
+    }
+}
