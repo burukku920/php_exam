2e5d9aa26788c797983497f92bcd0da52bba6276
sofiane1234 <79451497+sofiane1234@users.noreply.github.com>
2022-01-23 20:11:40 +0100

    Add files via upload

---
+++ actions/articles/articlesList.php
@@ -0,0 +1,14 @@
+<?php
+require('actions/database.php');
+
+//Obtenir les articles en-dehors des recherches 
+$getAllArt = $db->query('SELECT id, auteurId, auteurName, titre, description, date FROM articles ORDER BY id DESC LIMIT 0,13'); 
+
+//Checker si une recherche a ete entree, si la barre de recherche n'est pas vide
+if(isset($_GET['search']) && !empty($_GET['search'])) {
+    $searchUser = $_GET['search'];
+
+    //Obtenir les articles selon la recherche en evitant de limiter les recherches
+    $getAllArt = $db->query('SELECT id, auteurId, auteurName, titre, description, date FROM articles WHERE titre LIKE "%'.$searchUser.'%" ORDER BY id DESC');
+    
+}
