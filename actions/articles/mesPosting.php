10ea499875342e873cdbd919e1f90a6309274cd9
sofiane1234 <79451497+sofiane1234@users.noreply.github.com>
2022-01-24 08:58:54 +0100

    Delete actions directory

--- actions/articles/mesPosting.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require('actions/database.php');
-
-$getPost = $db->prepare('SELECT id, titre, description, messages FROM articles WHERE auteurId = ? ORDER BY messages DESC');
-$getPost->execute(array($_SESSION['id']));
--- actions/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    
-    $db = new PDO('mysql:host=localhost;dbname=php_exam_db;charset=utf8;', 'root', '');
-}catch(Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
