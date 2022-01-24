01e6caf813f19a2af381209ca0f19fc2372c2257
sofiane1234 <79451497+sofiane1234@users.noreply.github.com>
2022-01-24 11:25:27 +0100

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light btn-warning">
   <div class="container-fluid">
-    <a class="navbar-brand btn-primary" href="#">Php_exam_forum</a>
+    <a class="navbar-brand btn-primary" href="home.php">Php_exam_forum</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
